9f0cea917f7bcb070d800cff019b173200046741
MJ <1005789164@qq.com>
2020-02-13 13:17:40 +0800

    modify

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 set -e
 
+REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 INPUT_FORCE=${INPUT_FORCE:-false}
 INPUT_TAGS=${INPUT_TAGS:-false}
-INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 _FORCE_OPTION=''
-REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
+_TAGS_OPTION=''
+
 
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${INPUT_GITHUB_TOKEN}" ] && {
     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+[ -z "${INPUT_USERNAME}" ] && {
     exit 1;
 };
 
@@ -19,11 +25,12 @@ if ${INPUT_FORCE}; then
 fi
 
 if ${TAGS}; then
-    _TAGS='--tags'
+    _TAGS_OPTION='--tags'
 fi
 
 cd ${INPUT_DIRECTORY}
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="https://${INPUT_USERNAME}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS_OPTION
+
