f7e456330ffdd30aae22eb9fffb446abd7d6ad60
MJ <1005789164@qq.com>
2020-02-14 21:42:00 +0800

    bug fix

--- start.sh
+++ start.sh
@@ -29,7 +29,7 @@ if $INPUT_FOLDER; then
     current=`date "+%Y-%m-%d %H:%M:%S"`
     timeStamp=`date -d "$current" +%s`
     #将current转换为时间戳，精确到毫秒
-    currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
+    currentTimeStamp=$((timeStamp*1000+10#(`date "+%N"`/1000000)))
     _FOLDER_OPTION=$currentTimeStamp
     mkdir -p ../d_tmp && cp -rpf * ../d_tmp
     mkdir -p $_FOLDER_OPTION && mv ../d_tmp/* $_FOLDER_OPTION
