3ab4507fa5bfb0ec6ee74d747d816775c67a76fe
MJ <1005789164@qq.com>
2020-02-18 16:09:07 +0800

    test

--- start.sh
+++ start.sh
@@ -2,11 +2,7 @@
 set -e
 
 INPUT_YOU_TOKEN=${INPUT_YOU_TOKEN:-$GITHUB_TOKEN}
-INPUT_USER_EMAIL=${INPUT_USER_EMAIL:-'action@github.com'}
-INPUT_USER_NAME=${INPUT_USER_NAME:-'GitHub Action'}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'./'}
-INPUT_FOLDER=${INPUT_FOLDER:-'dir_upload'}
-INPUT_COMMIT_MSG=${INPUT_COMMIT_MSG:-'Add changes'}
 INPUT_BRANCH=${INPUT_BRANCH:-'master'}
 INPUT_FORCE=${INPUT_FORCE:-false}
 INPUT_TAGS=${INPUT_TAGS:-false}
@@ -28,31 +24,9 @@ if $INPUT_TAGS; then
     _TAGS_OPTION='--tags'
 fi
 
-echo ${GITHUB_WORKSPACE} && echo ${INPUT_DIRECTORY}
-
 cd ${INPUT_DIRECTORY}
-
-if [ $(pwd) != ${GITHUB_WORKSPACE} ]; then
-    mkdir -p ${GITHUB_WORKSPACE}/${INPUT_FOLDER} && cp -rpf * ${GITHUB_WORKSPACE}/${INPUT_FOLDER}
-    cd ${GITHUB_WORKSPACE}/${INPUT_FOLDER}
-fi
-
-git config --local user.email "${INPUT_USER_EMAIL}"
-
-git config --local user.name "${INPUT_USER_NAME}"
 
 remote_repo="https://${INPUT_YOU_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git remote -v
+git push --force $remote_repo HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS_OPTION
 
-git add -f ./
-
-git commit -m "${INPUT_COMMIT_MSG}" -a
-
-git push --force $remote_repo ${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS_OPTION
-
-cd ${INPUT_DIRECTORY}
-
-if [ $(pwd) != ${GITHUB_WORKSPACE} ]; then
-    cd ${GITHUB_WORKSPACE} && rm -rf ${INPUT_FOLDER}
-fi
